92ae23a75e3e9006c0299e405e5202cd4c07e442
Felipe Osorio <fosorio@ciandt.com>
2025-05-23 10:27:41 -0300

    Initial commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Felipe Eduardo Campelo Ferreira Osorio on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
